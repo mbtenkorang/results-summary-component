56bc127c8e2a0fc0539dc36fa1aab13a7d5dd059
Moses Tenkorang <mbtenkorang@yahoo.com>
2024-03-02 22:02:58 +0000

    Update styles for button element

--- src/components/ButtonComponent.vue
+++ src/components/ButtonComponent.vue
@@ -14,7 +14,7 @@ const props = defineProps({
 </script>
 
 <template>
-    <div>
+    <div class="font-semibold text-lg text-neutral-white border bg-neutral-gray-blue text-center rounded-3xl py-3">
         <a :href="link_url">{{ link_label }}</a>
     </div>
 </template>
