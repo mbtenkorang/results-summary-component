77e01477fb06b93ff5e4e5dc224ca402e3e8428f
Moses Tenkorang <mbtenkorang@yahoo.com>
2024-03-03 12:29:27 +0000

    Minor fixes for styling. Add hover states

--- src/components/ButtonComponent.vue
+++ src/components/ButtonComponent.vue
@@ -15,7 +15,7 @@ const props = defineProps({
 
 <template>
     <div
-        class="font-semibold text-lg text-neutral-white bg-neutral-gray-blue text-center rounded-3xl py-3 lg:text-xl lg:py-4 lg:rounded-[1.75rem]">
+        class="font-semibold text-lg text-neutral-white bg-neutral-gray-blue text-center rounded-3xl py-3 lg:text-xl lg:py-4 lg:rounded-[1.75rem] hover:bg-gradient-to-b from-gradient-slate-blue to-gradient-royal-blue transition ease-in delay-150 cursor-pointer">
         <a :href="link_url">{{ link_label }}</a>
     </div>
 </template>
