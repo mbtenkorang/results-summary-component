2a35c6aceecf8e4c1c70ea71f71ba3a4bf307082
Moses Tenkorang <mbtenkorang@yahoo.com>
2024-03-03 07:07:48 +0000

    Remove border for button

--- src/components/ButtonComponent.vue
+++ src/components/ButtonComponent.vue
@@ -14,7 +14,7 @@ const props = defineProps({
 </script>
 
 <template>
-    <div class="font-semibold text-lg text-neutral-white border bg-neutral-gray-blue text-center rounded-3xl py-3">
+    <div class="font-semibold text-lg text-neutral-white bg-neutral-gray-blue text-center rounded-3xl py-3">
         <a :href="link_url">{{ link_label }}</a>
     </div>
 </template>
